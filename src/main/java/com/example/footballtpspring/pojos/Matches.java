e221f887262baefcd5add6b7f9d151b3334e7eae
AnnyBarrero <anny_barrero@hotmail.com>
2023-01-26 08:56:33 +0100

    update matches pojo

--- src/main/java/com/example/footballtpspring/pojos/Matches.java
+++ src/main/java/com/example/footballtpspring/pojos/Matches.java
@@ -1,6 +1,6 @@
 package com.example.footballtpspring.pojos;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +10,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Matches {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer pointsEquipe1;
+    private Integer pointsEquipe2;
+    private Long idStade;
+    private Long idEquipe1;
+    private Long idEquipe2;
+    private Long idJournee;
+
+    @ManyToOne
+    private Stade stade;
+
+    @ManyToOne
+    private Journee journee;
+
 }
