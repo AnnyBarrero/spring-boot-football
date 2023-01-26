e9f67286cec9e81ef96c7b9fea4a015253c38b8a
AnnyBarrero <anny_barrero@hotmail.com>
2023-01-26 08:10:35 +0100

    add bdd

---
+++ src/main/java/com/example/footballtpspring/FootballTpSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballtpspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTpSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballTpSpringApplication.class, args);
+    }
+
+}
